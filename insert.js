35e0063de01a78343fc6f4d8d9f3e8bacb533e2a
sevidal <46994521+sevidal@users.noreply.github.com>
2019-01-24 12:35:26 +0100

    Delete insert.js

--- insert.js
+++
@@ -1,7 +0,0 @@
-use knowtech2
- 
- for (var i = 0; i < 10000; i++) {
-    db.alumno.insertOne(
-        { IdAlumno: i, Nombre: "Nombre" + i.toString(), Apellidos: "Apellidos" + i.toString(), Dni: "Dni" + i.toString()}
-     )
- }
