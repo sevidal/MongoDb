abb4faf61d01c30c0d214733e8dc718af30a78b0
sevidal <46994521+sevidal@users.noreply.github.com>
2019-01-25 11:45:21 +0100

    Delete insert.js

--- insert.js
+++
@@ -1,7 +0,0 @@
-db = db.getSiblingDB('knowtech0');
- 
- for (var i = 0; i < 10000; i++) {
-    db.alumno.insertOne(
-        { IdAlumno: i, Nombre: 'Nombre' + i.toString(), Apellidos: 'Apellidos' + i.toString(), Dni: 'Dni' + i.toString()}
-     );
- }
